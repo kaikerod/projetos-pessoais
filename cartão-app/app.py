4c33c4f770f00e8277cd94651c906ed7177788ad
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-03-12 17:59:52 -0300

    Update app.py

--- cartão-app/app.py
+++ cartão-app/app.py
@@ -2,13 +2,20 @@ class CartaoDeCredito:
     def __init__(self, titular, limite_total, limite_disponivel, valor_fatura):
         self.titular = titular
         self.limite_total = limite_total
-        self.limite_disponivel = limite_disponivel
+        self._limite_disponivel = limite_disponivel
         self.valor_fatura = valor_fatura
 
+    def __str__(self):
+      return f"Cartão de Crédito de {self.titular}"
+    
+    @property
+    def limite_disponivel(self):
+      return self._limite_disponivel
+
     def comprar(self, valor):
-      if valor > self.limite_disponivel:
+      if valor > self._limite_disponivel:
           print("Erro: Saldo insuficiente")
           return False
-      self.limite_disponivel -= valor
+      self._limite_disponivel -= valor
       self.valor_fatura += valor
       return True
